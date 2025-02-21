fb7e20b2bc5dbd78d7fdea9c34be948d2d507882
sebastianpl1306 <pabonlopezsebastian@gmail.com>
2025-02-21 06:45:50 -0500

    Creación de sección main home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,17 @@ import { HeaderHome } from "@/components";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <HeaderHome/>
-    </main>
+      <main className="w-full md:w-4/5 m-auto text-white">
+        <div className="flex justify-between items-end mt-4">
+          <div>
+            <h1 className="text-5xl font-semibold">Centro de Control</h1>
+            <p className="text-sm font-normal">Esto es lo que tenemos para ti el día de hoy.</p>
+          </div>
+          <button className="bg-primary rounded-lg py-2 px-8 font-semibold text-black">Recargar Saldo</button>
+        </div>
+      </main>
+    </>
   );
 }
